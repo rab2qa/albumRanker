494f5e2ce4b6995883c507cb52ca8bfed8b20030
Jeff Jacobs <jeff@jeffjacobs.net>
2018-12-03 20:44:55 -0600

    Failing attempts to filter data

--- src/app/classes/container.ts
+++ src/app/classes/container.ts
@@ -31,47 +31,100 @@ const defaultPageSize = 25;
 ///////////////////////
 
 export class Container<T> implements Pagable {
+  /**************/
+  /* PROPERTIES */
+  /**************/
 
-    /**************/
-    /* PROPERTIES */
-    /**************/
+  public paginationOptions: PaginationOptions;
 
-    public paginationOptions: PaginationOptions;
+  /***************/
+  /* CONSTRUCTOR */
+  /***************/
 
-    /***************/
-    /* CONSTRUCTOR */
-    /***************/
+  public constructor(private _name: string, private _data: Array<T>) {
+    this.paginationOptions = new PaginationOptions(
+      null,
+      false,
+      false,
+      this._data.length,
+      0,
+      defaultPageSize,
+      [10, 25, 50, 100],
+      true
+    );
+  }
 
-    public constructor(
-        private _name: string,
-        private _data: Array<T>
-    ) {
-        this.paginationOptions = new PaginationOptions(null, false, false, this._data.length, 0, defaultPageSize, [10, 25, 50, 100], true);
-    }
+  /*************/
+  /* ACCESSORS */
+  /*************/
 
-    /*************/
-    /* ACCESSORS */
-    /*************/
+  get all(): Array<T> {
+    return this._data;
+  }
 
-    get all(): Array<T> {
-        return this._data;
-    }
+  get page(): Array<T> {
+    const testFilters = {
+      name: 'Children',
+      year: '2011',
+    };
+    const filteredData = this.filterPageData(this._data, testFilters);
+    const start = this.paginationOptions.pageSize * this.paginationOptions.pageIndex;
+    const end = start + this.paginationOptions.pageSize;
+    return filteredData.slice(start, end);
+  }
 
-    get page(): Array<T> {
-        const start = this.paginationOptions.pageSize * this.paginationOptions.pageIndex;
-        const end = start + this.paginationOptions.pageSize;
-        return this._data.slice(start, end);
-    }
+  get name(): string {
+    return this._name;
+  }
 
-    get name(): string { return this._name; }
+  /******************/
+  /* PUBLIC METHODS */
+  /******************/
 
-    /******************/
-    /* PUBLIC METHODS */
-    /******************/
+  public onPageChange(event: PageEvent): void {
+    this.paginationOptions.pageIndex = event.pageIndex;
+    this.paginationOptions.pageSize = event.pageSize;
+  }
 
-    public onPageChange(event: PageEvent): void {
-        this.paginationOptions.pageIndex = event.pageIndex;
-        this.paginationOptions.pageSize = event.pageSize;
-    }
+  /******************/
+  /* PRIVATE METHODS */
+  /******************/
 
-}  // End class Container
+  public filterPageData(data: Array<T>, filters: any): Array<T> {
+    console.log(filters);
+    return data.filter(rankable => {
+      let requiresMatch = 0;
+      let hasMatch = 0;
+      const type = filters.type;
+      Object.keys(filters).forEach(key => {
+        if (filters[key]) {
+          requiresMatch++;
+          const filterValue = filters[key];
+          const rankableValue = rankable[key];
+          if (rankableValue && rankableValue.indexOf(filterValue) >= 0) {
+            hasMatch++;
+          } else {
+            const artist = rankable['artist'];
+            const album = rankable['albums'];
+            if (artist && artist.name.indexOf(filterValue) >= 0) {
+              hasMatch++;
+            } else if (album && album.name.indexOf(filterValue) >= 0) {
+              hasMatch++;
+            }
+          }
+          console.log(filterValue, rankableValue, requiresMatch);
+        }
+      });
+      console.log(requiresMatch, hasMatch);
+      // const name = rankable['name'] ? rankable['name'].name : undefined;
+      // const year = rankable['year'] ? rankable['year'] : undefined;
+      // if (name && name.indexOf(filters.name)) {
+      //   hasMatch = true;
+      // }
+      // if (year && filters.year.indexOf(year) >= 0) {
+      //   hasMatch = true;
+      // }
+      return hasMatch === requiresMatch;
+    });
+  }
+} // End class Container
