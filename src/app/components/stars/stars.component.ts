e57600b4d802d7d3727c7d71289f45473259c4ab
Jeff Jacobs <jeff@jeffjacobs.net>
2018-12-06 20:28:08 -0600

    Move rankingToStars method to stars component and change references

--- src/app/components/stars/stars.component.ts
+++ src/app/components/stars/stars.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ranker-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.scss'],
 })
-export class StarRatingsComponent {
+export class StarRatingsComponent implements OnInit {
   @Input() ratings: any[];
+  @Input() ranking: number;
+  public stars: number[];
   constructor() {}
+  ngOnInit() {
+    this.stars = this.rankingToStars(this.ranking);
+  }
+  public rankingToStars(ranking: number): number[] {
+    const fixedRanking = ranking.toFixed(2);
+    const array = [0, 0, 0, 0, 0];
+    const ratingWhole = +fixedRanking.toString().split('.')[0];
+    const ratingDecimal = +fixedRanking.toString().split('.')[1];
+    for (let i = 0; i < ratingWhole; i++) {
+      array[i] = 100;
+    }
+    if (ratingWhole < 5) {
+      array[ratingWhole] = ratingDecimal;
+    }
+    return array;
+  }
 }
